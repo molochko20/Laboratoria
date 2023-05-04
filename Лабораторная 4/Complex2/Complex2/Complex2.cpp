adaf1fd297029af4419c42876075e1a3994ea50e
molochko20 <102619913+molochko20@users.noreply.github.com>
2023-05-04 18:34:46 +0300

    Add files via upload

---
+++ Лабораторная 4/Complex2/Complex2/Complex2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+float fun_el(int i, float z) {
+	float f;
+	f = powl(z, -i) / sin(i * z);
+	return f;
+}
+
+float SumR(int n, float x) {
+	float sum = 0;
+	for (unsigned int k = 0; k < n; k++) {
+		sum += fun_el(k, x);
+	}
+	return sum;
+}
+
+int main(int argc, char** argv)
+{
+	int n = 100;
+	float x = 0.5;
+	float R = SumR(n, x);
+	cout << "Result: " << R << endl;
+	return 0;
+}
